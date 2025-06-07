5ca87c3a68ec52dcb774b67444fa4ccb86140ff1
AmeeYuki <yukinooame@gmail.com>
2025-06-08 00:55:32 +0700

    update env

--- api/baseApi.js
+++ api/baseApi.js
@@ -1,11 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { EXPO_PUBLIC_API_URL } from "@env"; // 👈 import từ .env
 
 // Base API config
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     // baseUrl: "http://192.168.1.165:5000/api",
-    baseUrl: "https://mean-capsuleroom-webapp.azurewebsites.net/api",
+    // baseUrl: "https://mean-capsuleroom-webapp.azurewebsites.net/api",
+    baseUrl: EXPO_PUBLIC_API_URL,
+
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
